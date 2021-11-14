537b7e2ae5f03e57afe0fdb91d329c31dfce634f
hiteshSVSK <hiteshsunkara@eworldes.com>
2021-11-14 18:28:50 +0530

    commit push

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav"
+import Footer from "./Footer"
+const Layout = (props) =>{
+return(
+   <>
+   <Nav/>
+   {props.children}
+   <Footer/>
+   </> 
+)
+}
+export default Layout
---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout/Layout"
+
+const Contact =() =>{
+return(
+    <>
+    <div>
+        <Layout>
+            
+        </Layout>
+    </div>
+    </>
+)
+}
+export default Contact
